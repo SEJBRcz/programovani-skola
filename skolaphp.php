c70054d2fe6a86dca1d83ef577326826ea1c2934
farma <farma@DESKTOP-BH43L70.home>
2020-03-27 13:37:26 +0100

    zatim asi nic

---
+++ skolaphp.php
@@ -0,0 +1,30 @@
+<html lang="cs">
+    <head> 
+        <title lang="cs">Vít Voráček ZT2B 2019-2020</title>;
+
+    </head>;
+    <body style=background-color:white; >
+        <h1> <b><i> Mobilní telefony Dú na programování ZT2B  </b></i> </h1>
+    <hr width="50" size="10" color="red" align="center" noshade>
+    <img src="oneplus-7t-" alt="mobil">
+    <a>https://www.czc.cz/oneplus-7-pro-8gb-256gb-zlata/264448/produkt</a>
+    <a href="https://www.czc.cz/oneplus-7-6gb-128gb-seda/260809/produkt">One plus 7</a>
+    <a href="stranka.html"><img  src="strom.gif" alt="strom"></a>
+    <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.alza.cz%2Foneplus%2Fv8244.htm&psig=AOvVaw3uji6s9jJourvK4gBUZtkF&ust=1585387374775000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLiM5ayquugCFQAAAAAdAAAAABAE"><img  src="OnePlus-banner.jpg" alt="baner"></a>
+    <ol start="0">
+        <li value="13 890">OnePlus7</li>
+        <li value="19 999">OnePlus7 pro</li>
+    </ol>
+    <ul style="list-style-image: URL('puntik.gif')">
+      <li>položka s obrázkovou odrážkou</li>
+    </ul>
+    <input type="submit" value="idk" />
+    
+    
+    
+    
+    <?php
+    ?>
+</body> 
+
+</html>
