1eb58da29aa54b7d318a5fe04cb4996cb7e37e21
farma <farma@DESKTOP-BH43L70.home>
2020-09-09 20:09:24 +0200

    zatim asi nic

--- skolaphp.php
+++ skolaphp.php
@@ -1,30 +1,94 @@
-<html lang="cs">
-    <head> 
-        <title lang="cs">Vít Voráček ZT2B 2019-2020</title>;
+<?php
 
-    </head>;
-    <body style=background-color:white; >
-        <h1> <b><i> Mobilní telefony Dú na programování ZT2B  </b></i> </h1>
-    <hr width="50" size="10" color="red" align="center" noshade>
-    <img src="oneplus-7t-" alt="mobil">
-    <a>https://www.czc.cz/oneplus-7-pro-8gb-256gb-zlata/264448/produkt</a>
-    <a href="https://www.czc.cz/oneplus-7-6gb-128gb-seda/260809/produkt">One plus 7</a>
-    <a href="stranka.html"><img  src="strom.gif" alt="strom"></a>
-    <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.alza.cz%2Foneplus%2Fv8244.htm&psig=AOvVaw3uji6s9jJourvK4gBUZtkF&ust=1585387374775000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLiM5ayquugCFQAAAAAdAAAAABAE"><img  src="OnePlus-banner.jpg" alt="baner"></a>
-    <ol start="0">
-        <li value="13 890">OnePlus7</li>
-        <li value="19 999">OnePlus7 pro</li>
-    </ol>
-    <ul style="list-style-image: URL('puntik.gif')">
-      <li>položka s obrázkovou odrážkou</li>
-    </ul>
-    <input type="submit" value="idk" />
-    
-    
-    
-    
-    <?php
-    ?>
-</body> 
+ini_set('xdebug.var_display_max_depth', '10');
+ini_set('xdebug.var_display_max_children', '256');
+ini_set('xdebug.var_display_max_data', '1024');
+ini_set("xdebug.overload_var_dump", 1);
 
-</html>
+$michalSlanina1 = 1;
+$michalSlanina2 = 1;
+
+if ($michalSlanina1 = $michalSlanina2) {
+    echo "Slanina Michal";
+}
+
+echo "<br>";
+
+if ($michalSlanina1 === 1) {
+    echo "Michal";
+    if ($michalSlanina2 === 1) {
+        echo "&nbsp";
+        echo "Slanina";
+    }
+}
+
+echo "<br>";
+
+switch ($michalSlanina1) {
+    case '1':
+        echo "1";
+        break;
+
+    case '2':
+        echo "2";
+        break;
+
+    case '3':
+        echo "3";
+        break;  
+
+    case '4':
+        echo "4";
+        break;
+
+    case '5':
+        echo "5";
+        break;
+
+    case '6':
+        echo "A";
+        break;
+
+    default:
+        echo "N";
+        break;
+}
+
+echo "<br>";
+
+$co_delat_v_karantene = array(1 => "šachy", "piškvorky", "videohry","televize", "knihy", "filmy","stránky na internetu", "programování", "editace videií","spaní");
+
+var_dump ($co_delat_v_karantene);
+
+echo "<br>";
+
+$co_delat_v_karantene_s_popisem = array( "první úroveň",
+    array( "druhá úroveň",
+        0 => array(0 => "třetí úroveň"),
+        array("šachy" =>
+            array(1 => "přátelské partie", "vážné partie", "rapid šach", "bleskový šach", "korespondenční partie")),
+        array("piškvorky" =>
+            array(1 => "Gomoku", "Rendžu", "Šestvorky", "3D piškvorky", "Pentago")),
+        array("videohry" =>
+            array(1 => "simulátor", "strategické", "VR", "MMO", "FPS")),
+        array("televize" =>
+            array(1 => "zprávy", "pořady", "reklamy", "filmy", "seriály")),
+        array("knihy" =>
+            array(1 => "babička", "kytice", "bylo nás pět", "hamlet", "romeo a julie")),
+        array("filmy" =>
+            array(1 => "akční", "komedie", "romantické", "horrorové", "sci-fi")),
+        array("stránky na internetu" =>
+            array(1 => "technologie", "vaření", "součastná situlace", "politika", "domácnost")),
+        array("programování" =>
+            array(1 => "C++", "JS", "java", "PHP", "python")),
+        array("editace videií" =>
+            array(1 => "kompilace", "montáž", "filmová", "animace", "videoklip")),
+        array("spaní" =>
+            array(1 => "na boku", "na zádech", "na břiše", "na zemi", "v sedě",)),
+    ),
+);
+
+
+var_dump ($co_delat_v_karantene_s_popisem);
+
+?>
