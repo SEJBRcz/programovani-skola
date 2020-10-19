e649ad05de4309d9eca9613dd8473bcbbfb5edd1
farma <farma@DESKTOP-BH43L70.home>
2020-10-19 09:13:49 +0200

    zatim asi nic

---
+++ requaireOOP2.php
@@ -0,0 +1,21 @@
+<?php
+require 'OOP.2.php';
+
+
+echo $user->getGameVítVoráček();
+
+echo $user->getTridaVítVoráček();
+
+echo $user->getHairVítVoráček();
+
+echo $user->getNameVítVoráček();
+
+echo $user->getLastnameVítVoráček();
+
+echo $user->getAnimeVítVoráček();
+
+echo $user->getMangaVítVoráček();
+
+echo $user->getNamedogVítVoráček();
+
+var_dump($user);
