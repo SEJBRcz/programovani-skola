5c846306a6d7cd8993e11da29c3540a4d8a1a573
farma <farma@DESKTOP-BH43L70.home>
2020-11-23 10:40:26 +0100

    zatim asi nic

--- generování pinu.php
+++ generování pinu.php
@@ -1,20 +1,8 @@
 <?php
+      for  ($x = 0;$x < 4; $x+=1){
+for ($i =0; $i< 4;$i+=1){
+echo(round(rand(1, 9)) );
 
-    // PHP >= 7
-    function str_rand(int $length = 4){
-        $x = '';
-        for($i = 1; $i <= $length; $i++){
-            $x .= dechex(rand(0,255));
-        }
-        return substr($x, 0, $length);
-        
-        return substr($x, 1, $length);
-        
-        return substr($x, 2, $length);
-        
-        return substr($x, 3, $length);
-        
-        return substr($x, 4, $length);
-    }
-    var_dump(str_rand());
+
+}echo("<br>");}
 ?>
