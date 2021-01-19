5d3c625695ad29cfe2708a363dde1a20a27eb93d
SEJBRcz <farmar99999@seznam.cz>
2021-01-19 12:24:27 +0100

    Update fun_two_params.php

--- fun_two_params.php
+++ fun_two_params.php
@@ -1,31 +1,38 @@
 
 <?php
 
-#Obsah lekce: Kombinace PHP a PHP OOP  vytvořit funkci jménem getPrice() s prefixem jmena a příjmení  bude dostávat dva parametry. První parametr bude cena, decimal a druhy parametr bude string, mena "CZK".  Funkce bude vracet měnu na dvě des. místa. Přes funkci number_format() bude cena formátovaná na dvě des. místa. Oddělení tisíců bude mezera a des. číslo bude děleno čárkou.  Funkce to bude vracet jako return na obrazovku se vypíse pomoci echo getPrice(par1, par2);  přiklad použití: number_format(100.58545, 2, ',', '.'); round(100.58545, 3);  použijete funkce php round(), a number_format()  Na moodle print screen prohlížeče a naGitlab odevzdej kód.
+require 'classes/UserVitVoracek.php';
+require 'classes/ZdrojVitVoracek.php';
 
+use VitVoraceklUser\VitVoracekUtils;
 
-#vars
+$zdroj = new VitVoracekZdroj\ZdrojVitVoracek;
 
-$par1 = 27.60;
-$xy = $par1;
-$par2 = "CZK";
-
-
-#code
-
-function VitVoracekgetPrice($x, $y)
-{
-
- echo $y;
- echo "<br>";
- return number_format($x, 2,',','.');
- 
-};
-
-
-
-echo VitVoracekgetPrice($par1, $par2);
+echo "<p>Výpis variabilních symbolů</p>";
 echo "<br>";
-echo "rounded:";
-echo round($xy);
-?>
+// vypise 5 variabilnich symbolu zacinajicich 2020
+echo VitVoracekUtils::getVsVitVoracek(5);
+echo "<br>";
+echo VitVoracekUtils::getVsVitVoracek(75);
+echo "<br>";
+echo VitVoracekUtils::getVsVitVoracek(625);
+echo "<br>";
+echo VitVoracekUtils::getVsVitVoracek(3);
+echo "<br>";
+echo VitVoracekUtils::getVsVitVoracek(9);
+echo "<br>";
+echo "<br>";
+
+// Vypsani get metod napeti v indexu
+echo $zdroj->getVitVoracekNapeti110V();
+echo "<br>";
+echo $zdroj->getVitVoracekNapeti48V();
+echo "<br>";
+echo $zdroj->getVitVoracekNapeti12V();
+echo "<br>";
+echo $zdroj->getVitVoracekNapeti9V();
+echo "<br>";
+echo $zdroj->getVitVoracekNapeti4_5V();
+echo "<br>";
+echo $zdroj->getVitVoracekNapeti1_5V();
+echo "<br>";
